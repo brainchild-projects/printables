67fc70cb3024ae88bfcefa29c3910cd2e74370e5
Wayne Duran <asartalo@gmail.com>
2021-07-07 21:09:38 +0800

    fix: container for printables link

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { List, ListItem, ListItemText } from '@material-ui/core';
+import {
+  List, ListItem, ListItemText, Container,
+} from '@material-ui/core';
 import Hero from '../../components/Hero';
 
 const MainPage = (): JSX.Element => (
   <>
     <Hero title="Printables" subtitle="Printable materials for Education" />
-    <List>
-      <ListItem>
-        <ListItemText secondary="Generate a printable calendar for the month">
-          <Link to="/calendar">Calendar</Link>
-        </ListItemText>
-      </ListItem>
-    </List>
+    <Container>
+      <List>
+        <ListItem>
+          <ListItemText secondary="Generate a printable calendar for the month">
+            <Link to="/calendar">Calendar</Link>
+          </ListItemText>
+        </ListItem>
+      </List>
+    </Container>
   </>
 );
 
