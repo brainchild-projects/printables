616803b17330bca86014da8201c078b173ea2880
Wayne Duran <asartalo@gmail.com>
2021-07-07 16:11:29 +0800

    feat: menu button on app bar

--- src/components/PrintablesAppBar.tsx
+++ src/components/PrintablesAppBar.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import {
+  AppBar, IconButton, Toolbar, Typography,
+} from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import { makeStyles } from '@material-ui/core/styles';
 
-const PrintablesAppBar = (): JSX.Element => (
-  <AppBar>
-    <Toolbar>
-      <Typography variant="h6">Printables</Typography>
-    </Toolbar>
-  </AppBar>
-);
+const styles = makeStyles(() => ({
+  wrap: {
+    minHeight: 64,
+  },
+}));
+
+const PrintablesAppBar = (): JSX.Element => {
+  const classes = styles();
+  return (
+    <div className={classes.wrap}>
+      <AppBar>
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6">Printables</Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
 
 export default PrintablesAppBar;
