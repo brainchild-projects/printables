2948d493e534480a47698b8c0ffab178860b7e94
Wayne Duran <asartalo@gmail.com>
2022-10-25 21:26:29 +0800

    ci: warmup cache workaround

--- cypress/e2e/00_smoke_test.cy.ts
+++ cypress/e2e/00_smoke_test.cy.ts
@@ -1,6 +1,16 @@
-it('shows header', () => {
+it('shows basic content', () => {
   cy.visitHome();
   cy.contains('Printables');
   cy.contains('Math Worksheets');
   cy.contains('Copyright');
+
+  cy.findByRole('list', { name: /worksheets/i }).within(() => {
+    cy.findByRole('link', { name: /number grid/i }).click();
+  });
+
+  // The following is just for the server to warm up
+  // TODO: Fix this when we figure out how to solve this slow warm up phase
+  // eslint-disable-next-line cypress/no-unnecessary-waiting
+  cy.wait(5000);
+  cy.hasCustomizeFormHeading(/number grid/i);
 });
