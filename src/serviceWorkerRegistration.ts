72e0b44ae43bb0f5a8ff002fba3ebbba6d1ef868
Wayne Duran <asartalo@gmail.com>
2023-06-13 18:05:39 +0800

    fix: avoid nesting promises

--- src/serviceWorkerRegistration.ts
+++ src/serviceWorkerRegistration.ts
@@ -92,31 +92,29 @@ function registerValidSW(swUrl: string, config?: Config) {
     });
 }
 
-function checkValidServiceWorker(swUrl: string, config?: Config) {
+async function checkValidServiceWorker(swUrl: string, config?: Config) {
   // Check if the service worker can be found. If it can't reload the page.
-  fetch(swUrl, {
-    headers: { 'Service-Worker': 'script' },
-  })
-    .then((response) => {
-      // Ensure service worker exists, and that we really are getting a JS file.
-      const contentType = response.headers.get('content-type');
-      if (
-        response.status === 404
-        || (contentType && !contentType.includes('javascript'))
-      ) {
-        // No service worker found. Probably a different app. Reload the page.
-        // eslint-disable-next-line promise/no-nesting
-        void navigator.serviceWorker.ready
-          .then((registration) => registration.unregister())
-          .then(() => window.location.reload());
-      } else {
-        // Service worker found. Proceed as normal.
-        registerValidSW(swUrl, config);
-      }
-    })
-    .catch(() => {
-      console.log('No internet connection found. App is running in offline mode.');
-    });
+  try {
+    const response = await fetch(swUrl, { headers: { 'Service-Worker': 'script' } });
+
+    // Ensure service worker exists, and that we really are getting a JS file.
+    const contentType = response.headers.get('content-type');
+    if (
+      response.status === 404
+      || (contentType && !contentType.includes('javascript'))
+    ) {
+      // No service worker found. Probably a different app. Reload the page.
+      // eslint-disable-next-line promise/no-nesting
+      const registration = await navigator.serviceWorker.ready;
+      await registration.unregister();
+      window.location.reload();
+    } else {
+      // Service worker found. Proceed as normal.
+      registerValidSW(swUrl, config);
+    }
+  } catch (error) {
+    console.log('No internet connection found. App is running in offline mode.:', error);
+  }
 }
 
 function loadListener(config?: Config) {
@@ -125,7 +123,7 @@ function loadListener(config?: Config) {
 
     if (isLocalhost) {
       // This is running on localhost. Let's check if a service worker still exists or not.
-      checkValidServiceWorker(swUrl, config);
+      void checkValidServiceWorker(swUrl, config);
 
       // Add some additional logging to localhost, pointing developers to the
       // service worker/PWA documentation.
