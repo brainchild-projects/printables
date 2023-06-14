3b9c982d78f7802bc4c0befd5a83c2d627deb1e7
Wayne Duran <asartalo@gmail.com>
2023-06-14 16:51:04 +0800

    test: fix failing specs with cypress

--- cypress/e2e/00_smoke_test.cy.ts
+++ cypress/e2e/00_smoke_test.cy.ts
@@ -1,4 +1,3 @@
-/* eslint-disable cypress/no-unnecessary-waiting */
 it('shows basic content', () => {
   cy.visitHome();
   cy.contains('Printables');
@@ -9,10 +8,6 @@ it('shows basic content', () => {
     cy.findByRole('link', { name: /number grid/i }).click();
   });
 
-  // The following is just for the server to warm up
-  // TODO: Fix this when we figure out how to solve this slow warm up phase
-  cy.wait(5000);
   cy.hasCustomizeFormHeading(/number grid/i);
-  cy.wait(5000);
   cy.visitHome();
 });
